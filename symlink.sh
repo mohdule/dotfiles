08578d0d06a088ed6ca9964af56cd8a7cb0bc8bc
Mohannad Salah <mohdule117@gmail.com>
2019-08-18 11:32:07 +0200

    Added symlink to rofi

--- symlink.sh
+++ symlink.sh
@@ -12,6 +12,8 @@ ln -sf $HOME/.bin/dotfiles/i3/config ~/.config/i3/config
 # Polybar
 ln -sf $HOME/.bin/dotfiles/polybar/config ~/.config/polybar/config
 
+# Rofi
+ln -sf $HOME/.bin/dotfiles/rofi/config ~/.config/rofi/config
 
 # Xresources
 ln -sf $HOME/.bin/dotfiles/Xresources/.Xresources ~/.Xresources
