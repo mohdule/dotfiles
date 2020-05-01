85e55cb0e88980d4d5db9f83a5a5cb0dcf2d8bff
Mohannad Salah <mohdule117@gmail.com>
2020-05-01 22:00:35 +0200

    Added basic mpd and ncmpcpp configs

--- symlink.sh
+++ symlink.sh
@@ -24,6 +24,12 @@ ln -sf $HOME/.bin/dotfiles/polybar/config ~/.config/polybar/config
 # Rofi
 ln -sf $HOME/.bin/dotfiles/rofi/config ~/.config/rofi/config
 
+# MPD
+ln -sf $HOME/.bin/dotfiles/mpd/mpd.conf ~/.config/mpd/mpd.conf
+
+# ncmpcpp
+ln -sf $HOME/.bin/dotfiles/ncmpcpp/config ~/.config/ncmpcpp/config
+
 # Ranger
 ln -sf $HOME/.bin/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
 
